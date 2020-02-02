771ee2347a6bb5fea1571a3891e9eef662c67063
Dima Mamat <dmytriim@wix.com>
2020-02-02 10:38:43 +0200

    separated e2e tests from unit tests

--- test/requestMaker.spec.ts
+++ test/requestMaker.spec.ts
@@ -43,25 +43,4 @@ describe('defaultRequestMakerv1', () => {
                 .resolves.toBeDefined()
         })
     })
-
-
-
-    describe('real call with  params : function = GLOBAL_QUOTE, symbol = MSFT', () => {
-        const params : GlobalQuoteParams = {
-            "function" : FUNCTION.GLOBAL_QUOTE,
-            "symbol" : 'MSFT'
-        }
-
-        describe('format', () => {
-            it('should contain "Global Quote" property', () => {
-                return expect(defaultRequestMaker(params).then(res => res.data))
-                    .resolves.toHaveProperty('Global Quote')
-            })
-
-            it('should contain "Global Quote"."05. price" property with string value', () => {
-                return expect(defaultRequestMaker(params).then(res => res.data))
-                    .resolves.toHaveProperty(['Global Quote', '05. price'], expect.any(String))
-            })
-        })
-    })
 })
