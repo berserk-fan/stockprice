283b0191bf91296b048a3722ca316a0648aee0c6
Dima Mamat <dmytriim@wix.com>
2020-02-02 13:29:36 +0200

    added test for cached client as well as  tests for it

--- src/stockPriceClient.ts
+++ src/stockPriceClient.ts
@@ -1,5 +1,7 @@
-import {RequestMaker, FUNCTION, Price} from './types'
+import {RequestMaker, FUNCTION, Price, StringToString} from './types'
 import {defaultRequestMaker} from './requestMaker'
+import LRU from 'lru-cache'
+
 export interface StockPriceClient {
     getPrice(symbol: string, restParams ?: any) : Promise<Price>
 }
@@ -7,15 +9,40 @@ export interface StockPriceClient {
 class StockPriceClientImpl implements StockPriceClient{
     constructor(private requestMaker = defaultRequestMaker) {}
 
-    async getPrice(symbol: string, restParams ?: any) : Promise<Price> {
+    async getPrice(symbol: string, restParams ?: StringToString) : Promise<Price> {
         const baseParams= {
             "function" : FUNCTION.GLOBAL_QUOTE,
             "symbol": symbol,
         }
+
         const params = Object.assign(baseParams, restParams)
         const res = await this.requestMaker(params)
         return {price : res.data["Global Quote"]["05. price"]}
     }
+}
+
+class CachingStockPriceClient implements StockPriceClient {
+    private lruCache : LRU<string, Price> = new LRU(50)
+
+    constructor(private client: StockPriceClient) {}
+
+    getPrice(symbol: string, restParams?: any): Promise<Price> {
+        let key = JSON.stringify(arguments)
+        let cachedRes = this.lruCache.get(key)
+        if(cachedRes !== undefined) {
+            return Promise.resolve(cachedRes)
+        } else {
+            return this.client.getPrice(symbol, restParams)
+                .then((res) => {
+                    this.lruCache.set(key, res)
+                    return res
+                })
+        }
+    }
+}
+
+export function makeCachingClient(stockPriceClient : StockPriceClient) {
+    return new CachingStockPriceClient(stockPriceClient)
 }
 
 export function makeClient(requestMaker : RequestMaker) {
